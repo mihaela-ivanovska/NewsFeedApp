3ea7211eff6a902be6add43c01e8106e6d128a83
mihaela-ivanovska <mihaelaivanovska994@outlook.com>
2022-06-23 14:47:02 +0200

    Settings Screen

--- Navigation/navigation.tsx
+++ Navigation/navigation.tsx
@@ -1,20 +1,37 @@
-import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {createSharedElementStackNavigator} from 'react-navigation-shared-element';
-import {Feed} from '../src/screens/feed/feedScreen';
-import {NewsDetails} from '../src/screens/NewsDetails/NewDetailsScreen';
+import { NavigationContainer } from "@react-navigation/native";
+import React from "react";
+import { createSharedElementStackNavigator } from "react-navigation-shared-element";
+import { Feed } from "../src/screens/feed/feedScreen";
+import { NewsDetails } from "../src/screens/NewsDetails/NewDetailsScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {Settings} from '../src/components/Settings/Settings';
-import { createStackNavigator } from '@react-navigation/stack';
+import { Settings } from "../src/components/Settings/Settings";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
 const FeedStack = createSharedElementStackNavigator();
 const Stack = createStackNavigator();
 const MainTabs = createBottomTabNavigator();
 
 function HomeStackScreen() {
   return (
-    <MainTabs.Navigator screenOptions={{headerShown: false}}>
-      <MainTabs.Screen name="Feed" component={Feed} />
-      <MainTabs.Screen name="Settings" component={Settings} />
+    <MainTabs.Navigator screenOptions={{ headerShown: false }}>
+      <MainTabs.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarIcon: ({}) => (
+            <Ionicons color="#444" size={28} name="home-outline" />
+          ),
+        }}
+      />
+      <MainTabs.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          tabBarIcon: ({}) => (
+            <Ionicons color="#444" size={28} name="settings-outline" />
+          ),
+        }}
+      />
     </MainTabs.Navigator>
   );
 }
@@ -23,14 +40,15 @@ export const RootNavigation = () => {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="Feed"
-        screenOptions={{headerShown: false}}>
+        screenOptions={{ headerShown: false }}
+      >
         <Stack.Screen name="Feed" component={HomeStackScreen} />
         <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen
           name="NewsDetails"
           component={NewsDetails}
           options={{
-            cardStyleInterpolator: ({current: {progress}}) => {
+            cardStyleInterpolator: ({ current: { progress } }) => {
               return {
                 cardStyle: {
                   opacity: progress,
--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-import {View, Text} from 'react-native';
+import { View, Text, Pressable } from "react-native";
+import styles from './stylesSettings';
+import { Ionicons } from '@expo/vector-icons';
 export const Settings: React.FC = () => {
-    return (
-        <View>
-            <Text>EN</Text>
-            <Text>BG</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+        <View style={styles.row}>
+        <Text style={styles.title}>Select a Language</Text>
+        <Ionicons color='#444' size={28} name='ios-language-outline' />
+      </View>
+      <Pressable>
+        <Text style={[styles.text]}>EN</Text>
+      </Pressable>
+      <Pressable>
+        <Text style={[styles.text]}>BG</Text>
+      </Pressable>
+    </View>
+  );
 };
-
